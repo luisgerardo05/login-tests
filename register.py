261aa4e8ed4b37571286e652dd46ab2f12fee917
Luis Huerta <luisgerhuerta@gmail.com>
2021-11-09 15:42:20 -0600

    Renamed bad_mail_register_route task by invalid_email_register_route

--- register.py
+++ register.py
@@ -139,12 +139,26 @@ class WebsiteUser(HttpUser):
             else:
                 response.failure("Got status code " + str(response.status_code) + " instead of 400")
 
+    @task
+    def invalid_email_register_route(self):
+        name, username, email = create_user()
+        with self.client.post(
+            url = '/api/users/register',
+            data = json.dumps({
+                "name": name,
                 "email": username + ".ermiry.com",
                 "username": username,
                 "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b",
                 "confirm": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
             }),
-            name='bad_mail_register_route',
+            name='invalid_email_register_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+
     @task
     def bad_domain_register_route(self):
         name, username, email = create_user()
