b8c43181856125261a57ed57d402815b941d1089
Luis Huerta <luisgerhuerta@gmail.com>
2021-11-09 15:40:18 -0600

    Added task to send request without every field needed in register route

--- register.py
+++ register.py
@@ -25,12 +25,100 @@ class WebsiteUser(HttpUser):
                 response.failure("Got status code " + str(response.status_code) + " instead of 200")
 
     @task
-    def bad_mail_register_route(self):
+    def no_name_register_route(self):
+        name, username, email = create_user()
+        with self.client.post(
+            url = '/api/users/register',
+            data = json.dumps({
+                "email": email,
+                "username": username,
+                "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b",
+                "confirm": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
+            }),
+            name='no_name_register_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+    
+    @task
+    def no_email_register_route(self):
         name, username, email = create_user()
         with self.client.post(
             url = '/api/users/register',
             data = json.dumps({
                 "name": name,
+                "username": username,
+                "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b",
+                "confirm": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
+            }),
+            name='no_email_register_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+    
+    @task
+    def no_username_register_route(self):
+        name, username, email = create_user()
+        with self.client.post(
+            url = '/api/users/register',
+            data = json.dumps({
+                "name": name,
+                "email": email,
+                "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b",
+                "confirm": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
+            }),
+            name='no_username_register_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+
+    @task
+    def no_password_register_route(self):
+        name, username, email = create_user()
+        with self.client.post(
+            url = '/api/users/register',
+            data = json.dumps({
+                "name": name,
+                "email": email,
+                "username": username,
+                "confirm": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
+            }),
+            name='no_password_register_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+    
+    @task
+    def no_confirm_register_route(self):
+        name, username, email = create_user()
+        with self.client.post(
+            url = '/api/users/register',
+            data = json.dumps({
+                "name": name,
+                "email": email,
+                "username": username,
+                "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b",
+            }),
+            name='no_confirm_register_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+
                 "email": username + ".ermiry.com",
                 "username": username,
                 "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b",
