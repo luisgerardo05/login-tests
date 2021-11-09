a12891c78813f96972550f75967b6e111e318de4
Luis Huerta <luisgerhuerta@gmail.com>
2021-11-09 15:33:16 -0600

    Added task to send request without email & password fields

--- login.py
+++ login.py
@@ -35,6 +35,14 @@ class WebsiteUser(HttpUser):
             else:
                 response.failure("Got status code " + str(response.status_code) + " instead of 400")
 
+    @task
+    def no_email_login_route(self):
+        with self.client.post(
+            url = '/api/users/login',
+            data = json.dumps({
+                "password": "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
+            }),
+            name='no_email_login_route',
             catch_response = True
         ) as response:
             if response.status_code == 400:
@@ -51,6 +59,21 @@ class WebsiteUser(HttpUser):
                 "password": "erick.salas@verstand.com.mx"
             }),
             name='bad_pswd_login_route',
+            catch_response = True
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+            else:
+                response.failure("Got status code " + str(response.status_code) + " instead of 400")
+    
+    @task
+    def no_pswd_login_route(self):
+        with self.client.post(
+            url = '/api/users/login',
+            data = json.dumps({
+                "email": "erick.salas@verstand.com.mx",
+            }),
+            name='no_pswd_login_route',
             catch_response = True
         ) as response:
             if response.status_code == 400:
